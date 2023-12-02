4849ee14d9273d91a9e4ba5884e300752bdcd8b7
Hassanin Ahmed <sas1ni69@gmail.com>
2023-12-02 14:26:09 +0800

    day 2 part 2

--- 02/part_2.rb
+++ 02/part_2.rb
@@ -1,18 +1,12 @@
 class Main
   def self.run(lines)
     lines.map do |line|
-      _, sets = line.split(':', 2)
-
-      min_blue  = get_minimum_cubes_by_colour(sets, 'blue')
-      min_green = get_minimum_cubes_by_colour(sets, 'green')
-      min_red   = get_minimum_cubes_by_colour(sets, 'red')
-
-      min_blue * min_green * min_red
-    end.compact.sum
-  end
-
-  def self.get_minimum_cubes_by_colour(sets, color)
-    sets.scan(/(\d+) #{color}/).flatten.map(&:to_i).max
+      line.scan(/(\d+) (\w+)/)
+        .each_with_object({}) { |(number, color), hash| hash[color] ||= []; hash[color] << number.to_i }
+        .transform_values(&:max)
+        .values
+        .inject(:*)
+    end.sum
   end
 end
 
